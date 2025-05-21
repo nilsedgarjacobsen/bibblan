139c8be91c988e26b7a9c2cd4fe5323850368327
nilsedgar <nils.jacobsen@zocom.se>
2025-05-21 09:31:27 +0200

    initial commit with sqlitedatabasefile and important jpa and db settings in application.properties

---
+++ src/main/java/com/example/bibblan/BibblanApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibblan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibblanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BibblanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bibblan/BibblanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibblan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibblanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
